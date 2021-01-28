147bef5a035ce85601a2f14a471e5181ef0ff6c8
JAI <mail2jaibarathi@gmail.com>
2021-01-28 12:24:08 +0530

    apolo server

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+import { typeDefs, resovlers } from "./schema";
+
+
+const app = express();
+
+  const server = new ApolloServer({
+    
+    typeDefs,
+    resolvers
+  });
+
+  server.applyMiddleware({ app });
+
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
+
